c31ca73864d0eb1dfa87fbda7a39209036bb1ffd
Zubair94 <kazizubair1994@gmail.com>
2019-01-20 10:54:27 +0600

    project on hold

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyADorfIoxNJG7hXFhVYxPYjzkqPZEUIvvs",
-    authDomain: "meme-share-825ac.firebaseapp.com",
-    databaseURL: "https://meme-share-825ac.firebaseio.com",
-    projectId: "meme-share-825ac",
-    storageBucket: "meme-share-825ac.appspot.com",
-    messagingSenderId: "983085303483"
-  }
+  firebase: {}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyADorfIoxNJG7hXFhVYxPYjzkqPZEUIvvs",
-    authDomain: "meme-share-825ac.firebaseapp.com",
-    databaseURL: "https://meme-share-825ac.firebaseio.com",
-    projectId: "meme-share-825ac",
-    storageBucket: "meme-share-825ac.appspot.com",
-    messagingSenderId: "983085303483"
-  }
+  firebase: {}
 };
 
 /*
